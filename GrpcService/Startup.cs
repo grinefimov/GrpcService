a9216adf55ccde6203dd43144a18b3b19ff8f676
Grin <grinefimov@gmail.com>
2019-11-12 20:30:33 +0300

    Add GrpcClient

--- GrpcService/Startup.cs
+++ GrpcService/Startup.cs
@@ -25,11 +25,6 @@ namespace GrpcService
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
-
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                });
             });
         }
     }
